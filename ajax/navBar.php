e8850dc6676abbde360b06d19071ed01e97d34ac
Simone Grossi <grossisimone@outlook.it>
2017-02-01 01:51:33 +0100

    Refactroring e new nel footer
    Ho sostituito il nome del progetto
    Modificato un po’ il footer e la sua classe

--- ajax/navBar.php
+++ ajax/navBar.php
@@ -1,6 +1,6 @@
 <nav>
     <img src="images/logo.png">
-    <h1 id="logo">Openil</h1>
+    <h1 id="logo">YourCodes</h1>
     <ul>
         <li><a href="#">Prodotti</a></li>
         <li><a href="#">Credits</a></li>
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
     <head>
-        <title>Openil</title>
+        <title>YourCodes</title>
         <meta charset="UTF-8">
         <meta name="description" content="opensource mind make opensource code">
-        <meta name="keywords" content="openil,opensource,software">
-        <meta name="author" content="Openil">
+        <meta name="keywords" content="yourcodes,opensource,software">
+        <meta name="author" content="YourCodes">
         <link rel="stylesheet" href="style/index.css">
     </head>
     <body>
@@ -17,7 +17,10 @@
                     <h1>L'OpenSource è come la natura</h1>
                     <h1>Libero</h1>
                 </div>
-                <video autoplay preload="auto" muted loop><source src="https://goo.gl/wjtTWf"></video>
+                <video autoplay preload="auto" muted poster="images/videoPoster.png">
+                    <source src="https://goo.gl/wjtTWf">
+                    Your browser does not support the video tag.
+                </video>
             </div>
         </main>
         <footer id="extensionView">

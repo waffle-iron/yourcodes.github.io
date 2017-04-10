ea28a32ff1b991d01657ce92ff6a6d8c42709075
Simone Grossi <grossisimone@outlook.it>
2017-04-10 17:03:49 +0200

    Effettuate un po' di modiche sulla home del sito

--- js/index.js
+++ js/index.js
@@ -11,3 +11,12 @@ $(document).ready(function () {
         navBar.toggleClass("colorChange", $(this).scrollTop() > 10)
     });
 });
+
+var terms = ["sviluppa", "divertiti", "pubblica"];
+
+function rotateTerm() {
+    var ct = $("#rotate").data("term") || 0;
+    $("#rotate").data("term", ct == terms.length -1 ? 0 : ct + 1).text(terms[ct]).fadeIn()
+        .delay(2000).fadeOut(200, rotateTerm);
+}
+$(rotateTerm);

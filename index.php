98fe650baf7bd8a2a29b04bc49bad0a8219017d4
Simone Grossi <grossisimone@outlook.it>
2017-04-10 12:19:38 +0200

    Un po’ di cosette

--- index.php
+++ index.php
@@ -6,37 +6,72 @@
         <meta name="keywords" content="yourcodes,opensource,software">
         <meta name="author" content="YourCodes">
         <link rel="stylesheet" href="flesh/style/index.css">
+        <link rel="stylesheet" href="flesh/style/tile.css">
     </head>
     <body>
         <header>
             <?php include 'ajax/navBar.php';?>
         </header>
         <section>
-            <div class="container hero">
+            <div class="hContainer hero">
                 <div class="overlay">
                     <h1>L'OpenSource è come la natura</h1>
                     <h1>Libero</h1>
                 </div>
-                <video class="cinegraph" loop autoplay><source src="media/cinegraph.tablet.mp4" type="video/mp4">Your browser does not support the video tag</video>
+                <video class="cinegraph" loop autoplay poster="media/videoPoster.png"><source src="media/cinegraph.tablet.mp4" type="video/mp4">Your browser does not support the video tag</video>
             </div>
-            <div class="container middleContainer">
-                <div class="imageDiv">
-                    <svg class="firstSvg" enable-background="new 0 0 128 128" id="Layer_1" version="1.1" viewBox="0 0 128 128" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><circle cx="64" cy="64" fill="#4B5F83" id="circle" r="64"/><g id="icon"><path d="M95,96H33c-2.2,0-4-1.8-4-4V37c0-2.2,1.8-4,4-4h62c2.2,0,4,1.8,4,4v55C99,94.2,97.2,96,95,96z" fill="#FFFFFF" id="bg"/><g id="text"><rect fill="#E6E6E6" height="4" id="XMLID_2_" width="33" x="56" y="57"/><rect fill="#E6E6E6" height="4" id="XMLID_7_" width="33" x="56" y="65"/><rect fill="#E6E6E6" height="4" id="XMLID_8_" width="50" x="39" y="73"/><rect fill="#E6E6E6" height="4" id="XMLID_9_" width="50" x="39" y="81"/></g><rect fill="#22A7F0" height="12" id="img" width="12" x="39" y="57"/><path d="M95,33H33c-2.2,0-4,1.8-4,4v4.1V42v4h70v-4v-0.9V37C99,34.8,97.2,33,95,33z" fill="#E6E6E6" id="header"/><circle cx="36" cy="39.8" fill="#CF000F" id="red" r="2"/><circle cx="42" cy="39.8" fill="#E67E22" id="orange" r="2"/><circle cx="48" cy="39.8" fill="#26A65B" id="green" r="2"/></g></svg>
-                    <svg class="secondSvg" style="enable-background:new 0 0 500 500;" version="1.1" viewBox="0 0 500 500" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="computer-devices-responsive"><g><circle cx="250" cy="250" r="250" style="fill:#F4F1F0;"/><g id="_x32_3"><path d="M270.052,341.998l3.358,7.183c0,0,0,2.105-1.405,2.105c-1.399,0-79.085,0-80.49,0     c-1.399,0-1.747-2.105-1.747-2.105l3.353-7.183H270.052z M269.89,326.747h-76.97v13.493h76.97V326.747z M116.851,292.198V166.135     h223.253v14.366h14.643v-24.777c0-3.857-3.152-7.01-7.01-7.01h-237.95c-3.857,0-7.009,3.152-7.009,7.01v140.175v3.299v16.385     c0,4.318,3.152,7.872,7.009,7.872h188.107v-31.256H116.851z" style="fill:#675F58;"/><path d="M380.339,188.911h-57.156c-9.294,0-16.89,7.601-16.89,16.878v128.602     c0,9.288,7.596,16.895,16.89,16.895h57.156c9.288,0,16.884-7.606,16.884-16.895V205.789     C397.223,196.512,389.627,188.911,380.339,188.911z M341.368,193.842c0-0.564,0.934-1.037,2.072-1.037h16.64     c1.14,0,2.072,0.472,2.072,1.037v0.526c0,0.564-0.933,1.037-2.072,1.037h-16.64c-1.139,0-2.072-0.472-2.072-1.037V193.842z      M353.711,199.957c0,1.074-0.879,1.948-1.953,1.948c-1.074,0-1.953-0.874-1.953-1.948c0-1.085,0.879-1.959,1.953-1.959     C352.832,197.998,353.711,198.872,353.711,199.957z M362.152,342.703c0,1.145-0.933,2.083-2.072,2.083h-16.64     c-1.139,0-2.072-0.938-2.072-2.083v-1.036c0-1.14,0.934-2.067,2.072-2.067h16.64c1.14,0,2.072,0.928,2.072,2.067V342.703z      M390.218,334.392h-75.716V205.789h75.716V334.392z" style="fill:#FF931E;"/></g></g></g><g id="Layer_1"/></svg>
-                    <svg class="thirdSvg" enable-background="new 0 0 512 512" height="512px" version="1.1" viewBox="0 0 512 512" width="512px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="Layer_1"/><g id="Colors"><rect fill="#FFFFFF" height="267" stroke="#231F20" stroke-miterlimit="10" width="267" x="-779" y="-512"/><rect fill="#FBD50A" height="267" stroke="#231F20" stroke-miterlimit="10" width="267" x="-779" y="-244"/><rect fill="#C61934" height="267" stroke="#231F20" stroke-miterlimit="10" width="267" x="-779" y="23"/><rect fill="#96212C" height="267" stroke="#231F20" stroke-miterlimit="10" width="267" x="-779" y="290"/><rect fill="#202C3C" height="267" stroke="#231F20" stroke-miterlimit="10" width="267" x="-779" y="557"/><rect fill="#ED1C24" height="267" stroke="#231F20" stroke-miterlimit="10" width="267" x="-779" y="825"/></g><g id="Icon"><ellipse cx="258.375" cy="255.649" fill="#FBD50A" rx="254.776" ry="252.774"/></g><g id="Layer_4"><path d="M109,371V152.333c0-6.627,5.373-12,12-12h274.667c6.627,0,12,5.373,12,12V371" fill="#F2F2F2"/><path d="M109,371V155.333c0-6.627,5.373-12,12-12h268.667c6.627,0,12,5.373,12,12V371" fill="#202C3C"/><path d="M109,164v-11.667c0-6.627,5.373-12,12-12h274.667c6.627,0,12,5.373,12,12V164" fill="#FFFFFF"/><circle cx="118.625" cy="152.125" fill="#C61934" r="3.5"/><circle cx="128.625" cy="152.125" fill="#FBD50A" r="3.5"/><circle cx="138.625" cy="152.125" fill="#39B54A" r="3.5"/><text fill="#39B54A" font-family="'MyriadPro-Cond'" font-size="36" transform="matrix(1 0 0 1 118.625 199.6667)"/><rect fill="#39B54A" height="32" width="16.5" x="208.625" y="171.667"/></g></svg>
-                    <svg class="fourthSvg" enable-background="new 0 0 128 128" version="1.1" viewBox="0 0 128 128" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="Layer_1"><rect fill="#F4F5F5" height="1520" opacity="0" width="727.938" x="-539.984" y="-731"/></g><g id="Layer_2"><g><circle cx="64" cy="64" fill="#F54F33" r="64"/><g><defs><circle cx="64" cy="64" id="SVGID_31_" r="64"/></defs><clipPath id="SVGID_2_"><use overflow="visible" xlink:href="#SVGID_31_"/></clipPath><polygon clip-path="url(#SVGID_2_)" fill="#DD2C24" points="20.161,96.119 52.036,128 128,128 128,52.536 108.115,32.622    "/></g><path d="M104.944,96.947H23.056c-2.209,0-4-1.791-4-4V35.053c0-2.209,1.791-4,4-4h81.888c2.209,0,4,1.791,4,4    v57.894C108.944,95.156,107.153,96.947,104.944,96.947z" fill="#FFFFFF"/><g><path d="M108.944,42.054v-7.001c0-2.209-1.791-4-4-4H23.056c-2.209,0-4,1.791-4,4v7.001H108.944z" fill="#E1E3E9"/></g><circle cx="25" cy="36.553" fill="#F24B45" r="3"/><circle cx="34" cy="36.553" fill="#FFB202" r="3"/><circle cx="43.001" cy="36.553" fill="#4CBB41" r="3"/><g><defs><path d="M104.944,96.947H23.056c-2.209,0-4-1.791-4-4V35.053c0-2.209,1.791-4,4-4h81.888c2.209,0,4,1.791,4,4      v57.894C108.944,95.156,107.153,96.947,104.944,96.947z" id="SVGID_33_"/></defs><clipPath id="SVGID_4_"><use overflow="visible" xlink:href="#SVGID_33_"/></clipPath><polygon clip-path="url(#SVGID_4_)" fill="#E1E3E9" opacity="0.8" points="30.866,70.855 57.438,97.5 110,97.5 110,79.563      97.134,66.663 84.469,63.719 71.795,51.056 64.376,67.188 53.475,56.3 31.937,68.83    "/></g><g><path d="M30.866,66.804L53.475,56.3v4.993l-17.099,7.49v0.094l17.099,7.489v4.993L30.866,70.855V66.804z" fill="#53899F"/><path d="M56.958,85.206l10.08-34.15h4.758l-10.081,34.15H56.958z" fill="#53899F"/><path d="M97.134,70.997L74.525,81.359v-4.993L92,68.877v-0.094l-17.476-7.49V56.3l22.609,10.363V70.997z" fill="#53899F"/></g></g></g></svg>
+            <div class="hContainer mid_H_Container">
+                <div class="tileContainer">
+                    <div class="box25">
+                        <img src="media/brainstorming.png">
+                        <h1>Progetta</h1>
+                    </div>
+                    <div class="box25">
+                        <img src="media/teamwork.png">
+                        <h1>Collabora</h1>
+                    </div>
+                    <div class="box25">
+                        <img src="/media/opensource.png">
+                        <h1>Condividi</h1>
+                    </div>
+                    <div class="box25">
+                        <img src="/media/missile.png">
+                        <h1>Fatti conoscere</h1>
+                    </div>
                 </div>
-                <div class="textDiv">
-                    <p>Quando scopri di avere una passione il bello è condividerla<br>Scrivere programmi oltre che un lavoro può essere anche un hobby</p>
-                </div>
+<!--                <div class="imageDiv">-->
+<!--                    <svg class="firstSvg" enable-background="new 0 0 128 128" id="Layer_1" version="1.1" viewBox="0 0 128 128" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><circle cx="64" cy="64" fill="#4B5F83" id="circle" r="64"/><g id="icon"><path d="M95,96H33c-2.2,0-4-1.8-4-4V37c0-2.2,1.8-4,4-4h62c2.2,0,4,1.8,4,4v55C99,94.2,97.2,96,95,96z" fill="#FFFFFF" id="bg"/><g id="text"><rect fill="#E6E6E6" height="4" id="XMLID_2_" width="33" x="56" y="57"/><rect fill="#E6E6E6" height="4" id="XMLID_7_" width="33" x="56" y="65"/><rect fill="#E6E6E6" height="4" id="XMLID_8_" width="50" x="39" y="73"/><rect fill="#E6E6E6" height="4" id="XMLID_9_" width="50" x="39" y="81"/></g><rect fill="#22A7F0" height="12" id="img" width="12" x="39" y="57"/><path d="M95,33H33c-2.2,0-4,1.8-4,4v4.1V42v4h70v-4v-0.9V37C99,34.8,97.2,33,95,33z" fill="#E6E6E6" id="header"/><circle cx="36" cy="39.8" fill="#CF000F" id="red" r="2"/><circle cx="42" cy="39.8" fill="#E67E22" id="orange" r="2"/><circle cx="48" cy="39.8" fill="#26A65B" id="green" r="2"/></g></svg>-->
+<!--                    <svg class="secondSvg" style="enable-background:new 0 0 500 500;" version="1.1" viewBox="0 0 500 500" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="computer-devices-responsive"><g><circle cx="250" cy="250" r="250" style="fill:#F4F1F0;"/><g id="_x32_3"><path d="M270.052,341.998l3.358,7.183c0,0,0,2.105-1.405,2.105c-1.399,0-79.085,0-80.49,0     c-1.399,0-1.747-2.105-1.747-2.105l3.353-7.183H270.052z M269.89,326.747h-76.97v13.493h76.97V326.747z M116.851,292.198V166.135     h223.253v14.366h14.643v-24.777c0-3.857-3.152-7.01-7.01-7.01h-237.95c-3.857,0-7.009,3.152-7.009,7.01v140.175v3.299v16.385     c0,4.318,3.152,7.872,7.009,7.872h188.107v-31.256H116.851z" style="fill:#675F58;"/><path d="M380.339,188.911h-57.156c-9.294,0-16.89,7.601-16.89,16.878v128.602     c0,9.288,7.596,16.895,16.89,16.895h57.156c9.288,0,16.884-7.606,16.884-16.895V205.789     C397.223,196.512,389.627,188.911,380.339,188.911z M341.368,193.842c0-0.564,0.934-1.037,2.072-1.037h16.64     c1.14,0,2.072,0.472,2.072,1.037v0.526c0,0.564-0.933,1.037-2.072,1.037h-16.64c-1.139,0-2.072-0.472-2.072-1.037V193.842z      M353.711,199.957c0,1.074-0.879,1.948-1.953,1.948c-1.074,0-1.953-0.874-1.953-1.948c0-1.085,0.879-1.959,1.953-1.959     C352.832,197.998,353.711,198.872,353.711,199.957z M362.152,342.703c0,1.145-0.933,2.083-2.072,2.083h-16.64     c-1.139,0-2.072-0.938-2.072-2.083v-1.036c0-1.14,0.934-2.067,2.072-2.067h16.64c1.14,0,2.072,0.928,2.072,2.067V342.703z      M390.218,334.392h-75.716V205.789h75.716V334.392z" style="fill:#FF931E;"/></g></g></g><g id="Layer_1"/></svg>-->
+<!--                    <svg class="thirdSvg" enable-background="new 0 0 512 512" height="512px" version="1.1" viewBox="0 0 512 512" width="512px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="Layer_1"/><g id="Colors"><rect fill="#FFFFFF" height="267" stroke="#231F20" stroke-miterlimit="10" width="267" x="-779" y="-512"/><rect fill="#FBD50A" height="267" stroke="#231F20" stroke-miterlimit="10" width="267" x="-779" y="-244"/><rect fill="#C61934" height="267" stroke="#231F20" stroke-miterlimit="10" width="267" x="-779" y="23"/><rect fill="#96212C" height="267" stroke="#231F20" stroke-miterlimit="10" width="267" x="-779" y="290"/><rect fill="#202C3C" height="267" stroke="#231F20" stroke-miterlimit="10" width="267" x="-779" y="557"/><rect fill="#ED1C24" height="267" stroke="#231F20" stroke-miterlimit="10" width="267" x="-779" y="825"/></g><g id="Icon"><ellipse cx="258.375" cy="255.649" fill="#FBD50A" rx="254.776" ry="252.774"/></g><g id="Layer_4"><path d="M109,371V152.333c0-6.627,5.373-12,12-12h274.667c6.627,0,12,5.373,12,12V371" fill="#F2F2F2"/><path d="M109,371V155.333c0-6.627,5.373-12,12-12h268.667c6.627,0,12,5.373,12,12V371" fill="#202C3C"/><path d="M109,164v-11.667c0-6.627,5.373-12,12-12h274.667c6.627,0,12,5.373,12,12V164" fill="#FFFFFF"/><circle cx="118.625" cy="152.125" fill="#C61934" r="3.5"/><circle cx="128.625" cy="152.125" fill="#FBD50A" r="3.5"/><circle cx="138.625" cy="152.125" fill="#39B54A" r="3.5"/><text fill="#39B54A" font-family="'MyriadPro-Cond'" font-size="36" transform="matrix(1 0 0 1 118.625 199.6667)"/><rect fill="#39B54A" height="32" width="16.5" x="208.625" y="171.667"/></g></svg>-->
+<!--                    <svg class="fourthSvg" enable-background="new 0 0 128 128" version="1.1" viewBox="0 0 128 128" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="Layer_1"><rect fill="#F4F5F5" height="1520" opacity="0" width="727.938" x="-539.984" y="-731"/></g><g id="Layer_2"><g><circle cx="64" cy="64" fill="#F54F33" r="64"/><g><defs><circle cx="64" cy="64" id="SVGID_31_" r="64"/></defs><clipPath id="SVGID_2_"><use overflow="visible" xlink:href="#SVGID_31_"/></clipPath><polygon clip-path="url(#SVGID_2_)" fill="#DD2C24" points="20.161,96.119 52.036,128 128,128 128,52.536 108.115,32.622    "/></g><path d="M104.944,96.947H23.056c-2.209,0-4-1.791-4-4V35.053c0-2.209,1.791-4,4-4h81.888c2.209,0,4,1.791,4,4    v57.894C108.944,95.156,107.153,96.947,104.944,96.947z" fill="#FFFFFF"/><g><path d="M108.944,42.054v-7.001c0-2.209-1.791-4-4-4H23.056c-2.209,0-4,1.791-4,4v7.001H108.944z" fill="#E1E3E9"/></g><circle cx="25" cy="36.553" fill="#F24B45" r="3"/><circle cx="34" cy="36.553" fill="#FFB202" r="3"/><circle cx="43.001" cy="36.553" fill="#4CBB41" r="3"/><g><defs><path d="M104.944,96.947H23.056c-2.209,0-4-1.791-4-4V35.053c0-2.209,1.791-4,4-4h81.888c2.209,0,4,1.791,4,4      v57.894C108.944,95.156,107.153,96.947,104.944,96.947z" id="SVGID_33_"/></defs><clipPath id="SVGID_4_"><use overflow="visible" xlink:href="#SVGID_33_"/></clipPath><polygon clip-path="url(#SVGID_4_)" fill="#E1E3E9" opacity="0.8" points="30.866,70.855 57.438,97.5 110,97.5 110,79.563      97.134,66.663 84.469,63.719 71.795,51.056 64.376,67.188 53.475,56.3 31.937,68.83    "/></g><g><path d="M30.866,66.804L53.475,56.3v4.993l-17.099,7.49v0.094l17.099,7.489v4.993L30.866,70.855V66.804z" fill="#53899F"/><path d="M56.958,85.206l10.08-34.15h4.758l-10.081,34.15H56.958z" fill="#53899F"/><path d="M97.134,70.997L74.525,81.359v-4.993L92,68.877v-0.094l-17.476-7.49V56.3l22.609,10.363V70.997z" fill="#53899F"/></g></g></g></svg>-->
+<!--                </div>-->
+<!--                <div class="textDiv">-->
+<!--                    <p>-->
+<!--                        Ho sempre voluto una vetrina dove mettere in bella vista le mie piccole creazioni, forse inutili alla massa, ma perché non andarne comunque fieri?-->
+<!--                        <br>-->
+<!--                        Non ho mai trovato nulla che fosse precisamente quello che cercavo, quindi, ho pensato di creare io quella vetrina.-->
+<!--                        <br>-->
+<!--                        Una sera però ragionando pensai che magari anche altre persone erano alla ricerca della stessa cosa e allora ho fondato il progetto YourCodes.-->
+<!--                        <br>-->
+<!--                        YourCodes è un'idea, una vetrina, un gruppo di amici, un insieme di sviluppatori, tante cose...-->
+<!--                        <br>-->
+<!--                        Qui tutti hanno la possibilità di avere la loro personale vetrina regolata solo dal buon senso ed il rispetto dell'ideale opensource.-->
+<!--                    </p>-->
+<!--                </div>-->
             </div>
-            <div class="container">
-                <div class="textDiv">
-                    <h1>Unisciti alla combricola</h1>
-                </div>
-                <div class="imageDiv">
-
-                </div>
+            <div class="hContainer">
+<!--                <div class="textDiv">-->
+<!--                    <p>-->
+<!--                        Unirsi alla banda è semplice, basta fare una richiesta e vi invieremo via mail tutte le informazioni.-->
+<!--                        <br>-->
+<!--                        Per la gestione dei repository ci appoggiamo a github ed in questo modo rispettiamo facilmente l'ideale opensource-->
+<!--                        <br>-->
+<!--                        Se vi piace il progetto è possibile donare tramite paypal-->
+<!--                    </p>-->
+<!--                </div>-->
+<!--                <div class="imageDiv">-->
+<!---->
+<!--                </div>-->
             </div>
         </section>
         <footer id="extensionView">
--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@ var navBar=$(".navBar");
 
 $(document).ready(function () {
     $(window).scroll(function () {
-        if ($(window).scrollTop() > 450) {
+        if ($(window).scrollTop() > 10) {
             $('nav').stop().animate({height: "80px"},250);
         }
         else {
             $('nav').stop().animate({height: "120px"},250);
         }
-        navBar.toggleClass("colorChange", $(this).scrollTop() > 450)
+        navBar.toggleClass("colorChange", $(this).scrollTop() > 10)
     });
 });
